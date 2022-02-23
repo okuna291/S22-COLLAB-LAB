b79bc9dae4a6c7e8c1302ca124fca815649838d5
ayo <okuna291@newschool.edu>
2022-02-23 13:49:54 -0500

    changes

--- nodeserial.js
+++ nodeserial.js
@@ -1,6 +1,24 @@
-const { SerialPort } = require('serialport')
-const { ReadlineParser } = require('@serialport/parser-readline')
-const port = new SerialPort({ path: 'COM4', baudRate: 9600 })
+// const SerialPort = require('serialport')
+// const Readline = require('@serialport/parser-readline')
+// const port = new SerialPort('/dev/cu.usbmodem14301')
 
-const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
-parser.on('data', console.log)
+// const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
+
+//  parser.on('data', data => {
+//     console.log(data)
+//   });
+
+// function readSerial() {
+// console.log(parser)}
+
+
+
+////or  the below
+
+
+// const { SerialPort } = require('serialport')
+// const { ReadlineParser } = require('@serialport/parser-readline')
+// const port = new SerialPort({ path: '/dev/cu.usbmodem14301', baudRate: 9600 })
+
+// const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
+// parser.on('data', console.log)
